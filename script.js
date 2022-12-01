fcde018676496f1d03cdcb133c7e50f4ffa98e86
Edouard de Romrée <deromree.edouard@gmail.com>
2022-12-01 12:06:09 +0100

    creation collection

---
+++ script.js
@@ -0,0 +1,156 @@
+const collection = [
+    {
+        name: "Avatar",
+        releaseYear: 2009,
+        genre: ["Science fiction"],
+        realisator: "James Cameron",
+        boxOffice: "2.922 milliards USD",
+        casting: 
+        [
+            "Zoe Saldana", 
+            "Sam Worthington",
+            "Sigourney Weaver",
+            "Michelle Rodriguez",
+        ]        
+    }
+    ,
+    {
+        name: "Interstellar",
+        releaseYear: 2014,
+        genre: ["Science fiction","Adventure"],
+        realisator: "Christopher Nolan",
+        boxOffice: "701.7 millions USD",
+        casting:
+        [
+            "Matt Demon",
+            "Anne Hathaway",
+            "Thimothée Chalamet",
+            "Matthew McConaughey",
+
+        ],
+    }
+    ,
+    {
+        name: "Black Panther",
+        releaseYear: 2018,
+        genre:["Action","Adventure"],
+        realisator: "Ryan Coogler",
+        boxOffice: "1.344 milliards USD",
+        casting:
+        [
+            "Chadwick Boseman",
+            "Michael B. Jordan",
+            "Letitia Wright",
+            "Angela Bassett",
+        ]
+
+    }
+    ,
+    {
+        name: "Sense 8",
+        duration: "2015-2018",
+        genre: ["Suspence","Science fiction"],
+        seasons: "2 seasons",
+        episodes: "24 episodes",
+        casting: 
+        [
+            "Jamie Clayton",
+            "Max Riemelt",
+            "Brian J. Smith",
+            "Miguel Ángel Silvestre",
+        ],
+    }
+    ,
+    {
+        name: "You",
+        duration: "2018 - Present",
+        genre: ["Thriller","Crime"],
+        seasons: "3 seasons",
+        episodes: "30 episodes",
+        casting:
+        [
+            "Penn Badgley",
+            "Victoria Pedretti",
+            "Tati Gabrielle",
+            "Jenna Ortega",
+        ],
+    }
+    ,
+    {
+        name: "Stranger Things",
+        duration: "2016 - Present",
+        genre: ["Horror","Suspence"],
+        seasons: "4 seasons",
+        episodes: "34 episodes",
+        casting:
+        [
+            "Millie Bobby Brown",
+            "Finn Wolfhard",
+            "Noah Schnapp",
+            "Caleb McLaughlin",
+        ]
+
+    }
+    ,
+    {
+        name: "Games of Thrones",
+        duration: "2011-2019",
+        genre: ["Fantasy","Adventure"],
+        seasons: "8 seasons",
+        episodes: "73 episodes",
+        casting:
+        [
+            "Emilia Clarke",
+            "Kit Harington",
+            "Sophie Turner",
+            "Maisie Williams",
+        ]
+    }
+    ,
+    {
+        name: "WandaVision",
+        duration: 2021,
+        genre: ["Science fiction","Action"],
+        seasons: "1 season",
+        episodes: "9 episodes",
+        casting:
+        [
+            "Elizabeth Olsen",
+            "Evan Peters",
+            "Kathryn Hahn",
+            "Kat Dennings",
+        ]
+    }
+    ,
+    {
+        name: "The Originals",
+        duration: "2013-2018",
+        genre: ["Horror","Mystery"],
+        seasons: "5 seasons",
+        episodes: "92 episodes",
+        casting: 
+        [
+            "Joseph Morgan",
+            "Daniel Gillies",
+            "Phoebe Tonkin",
+            "Claire Holt",
+        ]
+    }
+    ,
+    {
+        name: "Gossip Girl",
+        duration: "2007-2012",
+        genre: ["Romance","Drama"],
+        seasons: "6 seasons",
+        episodes: "121 episodes",
+        casting:
+        [
+            "Blake Lively",
+            "Leighton Meester",
+            "Ed Westwick",
+            "Chace Crawford",
+        ]
+    }
+
+]
+
