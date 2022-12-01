8cc1a88179a4ef71727a27ba6cd04580307b1845
Edouard de Romrée <deromree.edouard@gmail.com>
2022-12-01 17:02:09 +0100

    début css

--- script.js
+++ script.js
@@ -1,53 +1,58 @@
 const collection = [
     {
-        name: "Avatar",
-        releaseYear: 2009,
-        genre: ["Science fiction"],
-        realisator: "James Cameron",
-        boxOffice: "2.922 milliards USD",
+        name: "Chilling Adventures of Sabrina",
+        picture: "assets/Sabrina.jpg",
+        duration: "2018-2020",
+        genre: ["Horror","Suspence"],
+        seasons: "4 seasons",
+        episodes: "36 episodes",
         casting: 
         [
-            "Zoe Saldana", 
-            "Sam Worthington",
-            "Sigourney Weaver",
-            "Michelle Rodriguez",
-        ]        
+            "Kiernan Shipka", 
+            "Gavin Leatherwood",
+            "Ross Lynch",
+            "Michelle Gomez",
+        ]   
+        
     }
-    ,
-    {
-        name: "Interstellar",
-        releaseYear: 2014,
-        genre: ["Science fiction","Adventure"],
-        realisator: "Christopher Nolan",
-        boxOffice: "701.7 millions USD",
-        casting:
-        [
-            "Matt Demon",
-            "Anne Hathaway",
-            "Thimothée Chalamet",
-            "Matthew McConaughey",
 
-        ],
+    ,
+    {
+        name: "The hook Up Plan",
+        picture: "assets/Plan Coeur.jpg",
+        duration: "2018-2022",
+        genre: ["Romance","Comedy"],
+        seasons: "3 seasons",
+        episodes: "21 episodes",
+        casting:
+        [
+            "Zita Hanrot",
+            "Marc Ruchmann",
+            "Joséphine Draï",
+            "Sabrina Ouazani",
+        ]
     }
     ,
     {
-        name: "Black Panther",
-        releaseYear: 2018,
-        genre:["Action","Adventure"],
-        realisator: "Ryan Coogler",
-        boxOffice: "1.344 milliards USD",
+        name: "Santa Clarita Diet",
+        picture: "assets/SCD.jpg",
+        duration: "2017-2019",
+        genre:["Horror","Comedy"],
+        seasons: "3 seasons",
+        episodes: "30 episodes",
         casting:
         [
-            "Chadwick Boseman",
-            "Michael B. Jordan",
-            "Letitia Wright",
-            "Angela Bassett",
+            "Timothy Olyphant",
+            "Drew Barrymore",
+            "Liv Hewson",
+            "Skyler Gisondo"
         ]
 
     }
     ,
     {
         name: "Sense 8",
+        picture: "assets/sense8.jpg",
         duration: "2015-2018",
         genre: ["Suspence","Science fiction"],
         seasons: "2 seasons",
@@ -63,6 +68,7 @@ const collection = [
     ,
     {
         name: "You",
+        picture: "assets/you.jpg",
         duration: "2018 - Present",
         genre: ["Thriller","Crime"],
         seasons: "3 seasons",
@@ -78,6 +84,7 @@ const collection = [
     ,
     {
         name: "Stranger Things",
+        picture: "assets/Stranger.jpg",
         duration: "2016 - Present",
         genre: ["Horror","Suspence"],
         seasons: "4 seasons",
@@ -94,6 +101,7 @@ const collection = [
     ,
     {
         name: "Games of Thrones",
+        picture: "assets/GoT.jpg",
         duration: "2011-2019",
         genre: ["Fantasy","Adventure"],
         seasons: "8 seasons",
@@ -109,6 +117,7 @@ const collection = [
     ,
     {
         name: "WandaVision",
+        picture: "assets/wanda.jpg",
         duration: 2021,
         genre: ["Science fiction","Action"],
         seasons: "1 season",
@@ -124,6 +133,7 @@ const collection = [
     ,
     {
         name: "The Originals",
+        picture: "assets/Originals.jpg",
         duration: "2013-2018",
         genre: ["Horror","Mystery"],
         seasons: "5 seasons",
@@ -139,6 +149,7 @@ const collection = [
     ,
     {
         name: "Gossip Girl",
+        picture: "assets/xoxo.jpg",
         duration: "2007-2012",
         genre: ["Romance","Drama"],
         seasons: "6 seasons",
@@ -154,3 +165,48 @@ const collection = [
 
 ]
 
+for(let elem of collection){
+    let carte= document.createElement('div');
+    document.getElementsByClassName('container')[0].appendChild(carte);
+
+    //name
+    let names= document.createElement('h2');
+    names.textContent=elem['name'];
+    carte.appendChild(names);
+
+    //duration
+    let dur= document.createElement("p")
+    dur.classList.add("duration")
+    dur.textContent=elem['duration'];
+    carte.appendChild(dur)
+
+    //genre
+    let gen= document.createElement("p")
+    gen.classList.add("genre")
+    gen.textContent=elem['genre'];
+    carte.appendChild(gen)
+
+    //seasons
+    let sea= document.createElement("p")
+    sea.classList.add("seasons")
+    sea.textContent=elem['seasons'];
+    carte.appendChild(sea)
+
+    //episodes
+    let ep= document.createElement("p")
+    ep.classList.add("episodes")
+    ep.textContent=elem['episodes'];
+    carte.appendChild(ep)
+
+    //casting
+    let cast= document.createElement("p")
+    cast.classList.add("casting")
+    cast.textContent=elem['casting'];
+    carte.appendChild(cast)
+
+     //images
+     let image= document.createElement('img');
+     image.classList.add()
+     image.src=elem['picture'];
+     carte.appendChild(image);
+}
